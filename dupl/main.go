44c79e188edb2a48e73bbc7b075d5f62698ad451
sekthor <me@sekthor.dev>
2025-04-18 10:51:16 +0200

    feat: size based detection

--- dupl/main.go
+++ dupl/main.go
@@ -8,12 +8,15 @@ import (
 	"path/filepath"
 )
 
-var test filepath.WalkFunc
+var sizeIndex map[int64][]string = make(map[int64][]string)
 
 func index(path string, info fs.FileInfo, err error) error {
 	if info.IsDir() {
 		return nil
 	}
+
+	size := info.Size()
+	sizeIndex[size] = append(sizeIndex[size], path)
 	fmt.Println(path)
 
 	return nil
@@ -24,4 +27,10 @@ func main() {
 		log.Fatal("not enough arguments")
 	}
 	filepath.Walk(os.Args[1], index)
+
+	for _, v := range sizeIndex {
+		if len(v) > 1 {
+			fmt.Println("potential duplicates:", v)
+		}
+	}
 }
